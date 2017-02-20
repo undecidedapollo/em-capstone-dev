a4b639ee0a541c7a3a1e3f11471352ca6c3c9c56
Jonah Nestrick <jonah.nestrick@gmail.com>
2017-02-20 16:27:18 -0500

    BUILT WITH TIM!! Added UnitOfWork Builder to ease development using Moq

---
+++ EmployeeEvaluationSystem/EmployeeEvaluationSystem.Tests/Repositories/UserRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeEvaluationSystem.Entity;
+using EmployeeEvaluationSystem.Entity.SharedObjects.Repository.EF6;
+using EmployeeEvaluationSystem.Tests.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeEvaluationSystem.Tests.Repositories
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+
+        [TestMethod]
+        public void ThreeUserHasThreeUsers()
+        {
+
+            var x = new EFUnitOfWorkBuilder<EmployeeDatabaseEntities>();
+
+            x.InitializeOne(y => y.AspNetUsers, new List<AspNetUser>
+            {
+                new AspNetUser { FirstName = "BBB", LastName = "LLL", UserName = "123" },
+                new AspNetUser { FirstName = "CCC", LastName = "LLL", UserName = "456" },
+                new AspNetUser { FirstName = "DDD", LastName = "LLL", UserName = "789" }
+            });
+
+            using(var unitOfWork = new UnitOfWork(x.GetContext()))
+            {
+                var users = unitOfWork.Users.GetAllUsers(null).ToList();
+
+                unitOfWork.Complete();
+
+                Assert.AreEqual(3, users.Count());
+                Assert.AreEqual("BBB", users[0].FirstName);
+                Assert.AreEqual("CCC", users[1].FirstName);
+                Assert.AreEqual("DDD", users[2].FirstName);
+            }
+        }
+
+       
+    }
+}
