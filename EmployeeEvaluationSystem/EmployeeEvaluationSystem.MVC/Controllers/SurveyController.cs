ada6117982df5424ba22a5be2399206d4171805f
Jonah Nestrick <jonah.nestrick@gmail.com>
2017-03-01 17:04:27 -0500

    Started on the controller for survey page. Paused until clarification from teacher.

--- EmployeeEvaluationSystem/EmployeeEvaluationSystem.MVC/Controllers/SurveyController.cs
+++ EmployeeEvaluationSystem/EmployeeEvaluationSystem.MVC/Controllers/SurveyController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EmployeeEvaluationSystem.Entity;
+using EmployeeEvaluationSystem.Entity.SharedObjects.Repository.EF6;
+using EmployeeEvaluationSystem.MVC.Models.Survey;
+using EmployeeEvaluationSystem.SharedObjects.Extensions;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +14,27 @@ namespace EmployeeEvaluationSystem.MVC.Controllers
     public class SurveyController : Controller
     {
 
-        public ActionResult StartSurvey(int pendingSurveyId)
+        public ActionResult StartSurvey(Guid pendingSurveyId, string email = null)
         {
+            var userId = User?.Identity?.GetUserId();
 
 
+            if(userId == null && email == null)
+            {
+                throw new Exception();
+            }
 
+            bool guestMode = userId == null;
+
+            using (var unitOfWork = new UnitOfWork())
+            {
+                bool canTake = guestMode ? unitOfWork.Surveys.CanGuestUserTakeSurvey(email, pendingSurveyId): unitOfWork.Surveys.CanExistingUserTakeSurvey(userId, pendingSurveyId);
+
+                if (!canTake)
+                {
+                    throw new Exception();
+                }
+            }
 
             return View();
         }
@@ -23,12 +44,37 @@ namespace EmployeeEvaluationSystem.MVC.Controllers
             return View();
         }
 
-        public ActionResult SurveyPage()
+        public ActionResult SurveyPage(Guid pendingSurveyId, string email = null)
         {
+            var userId = User?.Identity?.GetUserId();
+
+
+            if (userId == null && email == null)
+            {
+                throw new Exception();
+            }
+
+            bool guestMode = userId == null;
+
+            using (var unitOfWork = new UnitOfWork())
+            {
+                bool canTake = guestMode ? unitOfWork.Surveys.CanGuestUserTakeSurvey(email, pendingSurveyId) : unitOfWork.Surveys.CanExistingUserTakeSurvey(userId, pendingSurveyId);
+
+                if (!canTake)
+                {
+                    throw new Exception();
+                }
+
+                var pendingSurvey = unitOfWork.Surveys.GetPendingSurveySYSTEM(pendingSurveyId).ThrowIfNull();
+
+
+
+            }
+
             return View();
         }
 
-        public ActionResult SurveyPage()
+        public ActionResult SurveyPage(SurveyPageViewModel model)
         {
             return View();
         }
