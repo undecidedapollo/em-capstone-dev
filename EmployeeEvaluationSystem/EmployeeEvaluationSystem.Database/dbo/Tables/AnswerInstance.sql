12f8cb1c3ff9f1efb9be8568d12c66e136afdaa4
Jonah Nestrick <jonah.nestrick@gmail.com>
2017-03-08 17:20:01 -0500

    Updated db

--- EmployeeEvaluationSystem/EmployeeEvaluationSystem.Database/dbo/Tables/AnswerInstance.sql
+++ EmployeeEvaluationSystem/EmployeeEvaluationSystem.Database/dbo/Tables/AnswerInstance.sql
@@ -1,9 +1,12 @@
-﻿CREATE TABLE [dbo].[AnswerInstance]
-(
-	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [QuestionID] INT NOT NULL, 
-    [ResponseNum] INT NOT NULL, 
-    [SurveyInstanceId] INT NOT NULL, 
-    CONSTRAINT [FK_AnswerInstance_SurveyInstance] FOREIGN KEY ([SurveyInstanceId]) REFERENCES [SurveyInstance]([ID]), 
-    CONSTRAINT [FK_AnswerInstance_To_Question] FOREIGN KEY ([QuestionID]) REFERENCES [Question]([ID])
-)
+﻿CREATE TABLE [dbo].[AnswerInstance] (
+    [ID]               INT IDENTITY (1, 1) NOT NULL,
+    [QuestionID]       INT NOT NULL,
+    [ResponseNum]      INT NOT NULL,
+    [SurveyInstanceId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_AnswerInstance_SurveyInstance] FOREIGN KEY ([SurveyInstanceId]) REFERENCES [dbo].[SurveyInstance] ([ID]),
+    CONSTRAINT [FK_AnswerInstance_To_Question] FOREIGN KEY ([QuestionID]) REFERENCES [dbo].[Question] ([ID]),
+    CONSTRAINT [AK_AnswerInstance_Column] UNIQUE NONCLUSTERED ([SurveyInstanceId] ASC, [QuestionID] ASC)
+);
+
+
