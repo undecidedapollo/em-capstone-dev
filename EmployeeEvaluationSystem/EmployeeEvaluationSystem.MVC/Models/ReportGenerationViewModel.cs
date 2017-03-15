f78290cf10378ede1cf19d882d9708573b19cbec
Tim Bergquist <tbergqu1@my.westga.edu>
2017-03-14 23:36:18 -0400

    Added new properties for Report Generation

--- EmployeeEvaluationSystem/EmployeeEvaluationSystem.MVC/Models/ReportGenerationViewModel.cs
+++ EmployeeEvaluationSystem/EmployeeEvaluationSystem.MVC/Models/ReportGenerationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeEvaluationSystem.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,21 @@ namespace EmployeeEvaluationSystem.MVC.Models
 {
     public class ReportGenerationViewModel
     {
+        public decimal OverallAverage { get; set; }
+        public decimal SuperviseeAverage { get; set; }
+        public decimal CoworkerAverage { get; set; }
+        public int SupervisorEvaluationResult { get; set; }
+        public int SelfEvaluationResult { get; set; }
+        public string Stage { get; set; }
+
+
+        public CohortUser CohortId { get; set; }
+        public CohortUser CohortUserId { get; set; }
+        public AspNetUser EmployeeId { get; set; }
+        public AnswerInstance QuestionId { get; set; }
+        public AnswerInstance RatingValue { get; set; }
+        public Category CategoryName { get; set; }
+        public AspNetUser Username { get; set; }
+       
     }
 }
