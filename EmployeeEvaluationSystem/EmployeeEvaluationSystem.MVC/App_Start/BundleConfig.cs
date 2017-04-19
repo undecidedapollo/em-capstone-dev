552e25ad82034982cba2bbd81f24726ded2539b1
Jonah Nestrick <jonah.nestrick@gmail.com>
2017-04-19 15:32:55 -0400

    Quick bug fix.

--- EmployeeEvaluationSystem/EmployeeEvaluationSystem.MVC/App_Start/BundleConfig.cs
+++ EmployeeEvaluationSystem/EmployeeEvaluationSystem.MVC/App_Start/BundleConfig.cs
@@ -9,8 +9,7 @@ namespace EmployeeEvaluationSystem.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,7 +25,7 @@ namespace EmployeeEvaluationSystem.MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/lumen.bootstrap.css",
-                      "~/Content/site.css", "~/Content/themes/base/minified/jquery-ui.min.css"));
+                      "~/Content/site.css"));
         }
     }
 }
