e5a6cbb7039d2e656ff8f547c1f460e7825f36e7
Bradley Norman <Bradley.Norman91@gmail.com>
2017-04-27 16:06:03 -0400

    Tests

--- EmployeeEvaluationSystem/EmployeeEvaluationSystem.Tests/Repositories/CohortRepositoryTest.cs
+++ EmployeeEvaluationSystem/EmployeeEvaluationSystem.Tests/Repositories/CohortRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EmployeeEvaluationSystem.Tests.Infrastructure;
 using EmployeeEvaluationSystem.Entity;
@@ -11,7 +12,7 @@ namespace EmployeeEvaluationSystem.Tests.Repositories
     public class CohortRepositoryTest
     {
         [TestMethod]
-        public void GetCohortWith1Returns1()
+        public void GetCohortWithIDReturnsCohortWithGivenID()
         {
             var x = EFUnitOfWorkBuilder<EmployeeDatabaseEntities>
                 .Create().InitializeOne(y => y.Cohorts, new List<Cohort>
@@ -20,7 +21,7 @@ namespace EmployeeEvaluationSystem.Tests.Repositories
                     {
                         ID = 5,
                         IsDeleted = false,
-                        Name="Bradley"
+                        Name = "Bradley"
                     }
                 });
 
@@ -34,9 +35,30 @@ namespace EmployeeEvaluationSystem.Tests.Repositories
             }
         }
 
+        [TestMethod]
+        public void GetCohortWithIDThatDoesNotExistReturnsNull()
+        {
+            var x = EFUnitOfWorkBuilder<EmployeeDatabaseEntities>
+                .Create().InitializeOne(y => y.Cohorts, new List<Cohort>
+                {
+                    new Cohort
+                    {
+                        ID = 5,
+                        IsDeleted = false,
+                        Name = "Bradley"
+                    }
+                });
+
+            using (var unitOfWork = new UnitOfWork(x.GetContext()))
+            {
+                var cohort = unitOfWork.Cohorts.GetCohort(null, 6);
+
+                Assert.IsNull(cohort);
+            }
+        }
 
         [TestMethod]
-        public void DeleteCohortTest()
+        public void DeleteCohortWithIDDeletesCorrectCohort()
         {
             var x = EFUnitOfWorkBuilder<EmployeeDatabaseEntities>
                 .Create().InitializeOne(y => y.Cohorts, new List<Cohort>
@@ -61,6 +83,39 @@ namespace EmployeeEvaluationSystem.Tests.Repositories
                 cohort = unitOfWork.Cohorts.GetCohort(null, 5);
 
                 Assert.IsNull(cohort);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteCohortWithIDThatDoesNotExistThrowsException()
+        {
+            var x = EFUnitOfWorkBuilder<EmployeeDatabaseEntities>
+                .Create().InitializeOne(y => y.Cohorts, new List<Cohort>
+                {
+                    new Cohort
+                    {
+                        ID = 5,
+                        IsDeleted = false,
+                        Name="Bradley"
+                    }
+                });
+
+            using (var unitOfWork = new UnitOfWork(x.GetContext()))
+            {
+                var cohort = unitOfWork.Cohorts.GetCohort(null, 5);
+
+                Assert.IsNotNull(cohort);
+                Assert.AreEqual(cohort.ID, 5);
+
+                try
+                {
+                    var isDeleted = unitOfWork.Cohorts.DeleteCohort(null, 6);
+                }
+                catch
+                {
+                    
+                }
+
             }
         }
 
@@ -115,16 +170,110 @@ namespace EmployeeEvaluationSystem.Tests.Repositories
             {
                 var cohort = new Cohort
                 {
-                    ID = 5,
+                    ID = 6,
                     IsDeleted = false,
                     Name = "Good",
                     Description = "Good"
                 };
 
-                cohort = unitOfWork.Cohorts.EditCohort(null, cohort);
+                unitOfWork.Cohorts.AddCohortToDb(null, cohort);
 
-                Assert.AreEqual(cohort.Name, "Good");
-                Assert.AreEqual(cohort.Description, "Good");
+                var cohorts = unitOfWork.Cohorts.GetAllCohorts(null).ToList();
+
+                Assert.AreEqual(cohorts.Count, 2);
+            }
+        }
+
+        [TestMethod]
+        public void GetAllCohortsTest()
+        {
+            var x = EFUnitOfWorkBuilder<EmployeeDatabaseEntities>
+                .Create().InitializeOne(y => y.Cohorts, new List<Cohort>
+                {
+                    new Cohort
+                    {
+                        ID = 5,
+                        IsDeleted = false,
+                        Name="Bad",
+                        Description = "Bad"
+                    },
+
+                    new Cohort
+                    {
+                        ID = 6,
+                        IsDeleted = false,
+                        Name="Bad",
+                        Description = "Bad"
+                    }
+                });
+
+            using (var unitOfWork = new UnitOfWork(x.GetContext()))
+            {
+               var cohorts = unitOfWork.Cohorts.GetAllCohorts(null).ToList();
+
+                Assert.AreEqual(cohorts.Count, 2);
+            }
+        }
+
+        [TestMethod]
+        public void GetAllUsersNotPartOfACohortTest()
+        {
+            var x = EFUnitOfWorkBuilder<EmployeeDatabaseEntities>
+                .Create().InitializeOne(y => y.AspNetUsers, new List<AspNetUser>
+                {
+                    new AspNetUser
+                    {
+                        Id = "1",
+                        FirstName = "Tod",
+                        LastName = "Rod",
+                        Email = "Tod.Rod@mailinator.com"
+                    },
+
+                    new AspNetUser
+                    {
+                        Id = "2",
+                        FirstName = "Jay",
+                        LastName = "Bay",
+                        Email = "Jay.Bay@mailinator.com"
+                    }
+                }).InitializeOne(y => y.Cohorts, new List<Cohort>
+                {
+                    new Cohort
+                    {
+                        ID = 1,
+                        Name = "TestCohort",
+                        Description = "TestDescription",
+                        IsDeleted = false
+                    }
+                }).InitializeOne(y => y.CohortUsers, new List<CohortUser>
+                {
+                    new CohortUser
+                    {
+                        UserID = "1",
+                    }
+                });
+
+            using (var unitOfWork = new UnitOfWork(x.GetContext()))
+            {
+                var cohort = unitOfWork.Cohorts.GetCohort(null, 1);
+
+                var cohortUser = unitOfWork.CohortUsers.GetCohortUser(null, "1");
+
+                var ASPNetUsers = unitOfWork.Users.GetAllUsers(null);
+
+                cohortUser.AspNetUser = ASPNetUsers.First();
+
+                cohortUser.Cohort = cohort;
+
+                cohort.CohortUsers.Add(cohortUser);
+
+                var usersNotInCohort = unitOfWork.Cohorts.GetAllUsersThatAreNotPartOfACohort(null);
+
+                Assert.AreEqual(usersNotInCohort.Count(), 1);
+
+                var user = usersNotInCohort.First();
+
+                Assert.AreEqual(user.Id, "2");
             }
         }
     }
